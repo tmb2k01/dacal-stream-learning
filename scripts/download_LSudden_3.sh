34cc9a197d12282512e5fa8bed4b6b0faa6c6cbf
Kristof Pasztor <pasztor.kristof.kp@gmail.com>
2026-03-30 19:57:31 +0000

    Experiment with LSudden_3 dataset

---
+++ scripts/download_LSudden_3.sh
@@ -0,0 +1,4 @@
++!/bin/bash
+
+mkdir data/
+curl -o data/LSudden_3.csv https://raw.githubusercontent.com/songqiaohu/THU-Concept-Drift-Datasets-v1.0/refs/heads/main/Datasets_in_CADM%2B/LSudden_3.csv
---
+++ scripts/download_SEA_concepts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir data/
+curl -o data/SEA_training_class.csv https://raw.githubusercontent.com/vlosing/driftDatasets/refs/heads/master/artificial/sea/SEA_training_class.csv
+curl -o data/SEA_training_data.csv https://raw.githubusercontent.com/vlosing/driftDatasets/refs/heads/master/artificial/sea/SEA_training_data.csv
